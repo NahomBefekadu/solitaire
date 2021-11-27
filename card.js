015e122944d08149e9101c31d3c99eb29d6bff9b
aanwar <TheNewFasl21#1>
2021-11-26 21:59:09 -0500

    Js updated

--- card.js
+++ card.js
@@ -1,13 +1,28 @@
 let suits = ["H", "C", "D", "S"];
 let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let deck = [];
+const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+ 
+createBoard();
+
+async function createBoard(){
+  //
+  
+  for (let j = 0; j < 28; j++) {
+    await sleepNow(30)
+    var newNode = document.createElement('div')
+    newNode.className = 'topCards'
+    document.getElementById('dd').appendChild(newNode)
+    //await sleepNow(1);
+  } 
+}
+ 
 
 for (let i = 0; i < 4; i++) {
   for (let j = 0; j < 13; j++) {
     deck.push([suits[i] + ranks[j]]);
   }
 }
-console.log(deck);
 
 let ShuffleDeck = function (DCK) {
   let var1, tmp;
@@ -21,6 +36,6 @@ let ShuffleDeck = function (DCK) {
   return DCK;
 };
 
-let newDeck = ShuffleDeck(deck);
 
-//console.log(newDeck);
+ShuffleDeck(deck);
+console.log(deck);
