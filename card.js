a8eecf2f1f7ad27f1e421d40c34a3b4e53e140e0
NahomBefekadu <58083518+NahomBefekadu@users.noreply.github.com>
2021-11-26 20:07:38 -0500

    update v2

---
+++ card.js
@@ -0,0 +1,24 @@
+let suits = ["H", "C", "D", "S"];
+let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let deck = [];
+
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 13; i++) {
+    deck.push([suits[i] + ranks[j]]);
+  }
+}
+console.log(deck);
+
+let ShuffleDeck = function (DCK) {
+  let var1, tmp;
+
+  for (let i = DCK.length - 1; i < 0; i--) {
+    var1 = Math.floor(Math.random() * (i + 1));
+    tmp = DCK[i];
+    DCK[i] = DCK[Var1];
+    DCK[Var1] = tmp;
+  }
+  return DCK;
+};
+
+let newDeck = shuffleDeck(deck);
