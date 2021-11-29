555ee983c1bf9f69b2088d98449d77a068dcafa3
NahomBefekadu <58083518+NahomBefekadu@users.noreply.github.com>
2021-11-29 16:28:16 -0500

    demo fix

--- card.js
+++ card.js
@@ -1,8 +1,11 @@
 let suits = ["H", "C", "D", "S"];
 let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let deck = [];
+var deckCopy = [];
 let topDeck = [];
+let topDeck2 = [];
 let waste = [];
+let joe = 0;
 const sleepNow = (delay) =>
   new Promise((resolve) => setTimeout(resolve, delay));
 
@@ -11,7 +14,7 @@ createBoard();
 async function createBoard() {
   //
 
-  for (let j = 0; j < 28; j++) {
+  for (let j = 0; j < 21; j++) {
     await sleepNow(30);
     var newNode = document.createElement("div");
     newNode.className = "topCards";
@@ -38,11 +41,26 @@ let ShuffleDeck = function (DCK) {
   return DCK;
 };
 
-ShuffleDeck(deck);
+deckCopy = ShuffleDeck(deck);
+console.log("deck");
 console.log(deck);
+topDeck = deckCopy;
+
+for (let i = 0; i < 24; i++) {
+  topDeck2.push(topDeck.shift());
+}
+
+console.log("deck");
+console.log(deck);
+console.log("topDeck");
+console.log(topDeck);
+console.log("topDeck2");
+console.log(topDeck);
 let n = 0;
+
 function pickCard() {
-  waste.push(deck.shift());
+  waste.push(topDeck2.shift());
+  console.log("picture is :" + waste[0 + n] + "number : " + n);
   let vs = `/images/cards/${waste[0 + n]}.svg`;
   n++;
   var newNode = document.createElement("div");
@@ -53,7 +71,36 @@ function pickCard() {
 
   var list = document.getElementById("dd"); // Get the <ul> element with id="myList"
   list.removeChild(list.childNodes[0]);
-  console.log(n);
+  console.log(waste.length);
+  console.log(topDeck2.length);
+
+  if (topDeck2.length === 0 && waste.length === 24) {
+    resetCard();
+  }
 }
 
+async function resetCard() {
+  //
+  for (let j = 0; j < waste.length; j++) {
+    await sleepNow(30);
+    var newNode = document.createElement("div");
+    newNode.className = "topCards";
+    document.getElementById("dd").appendChild(newNode);
+    //await sleepNow(1);
+    var list = document.getElementById("disc"); // Get the <ul> element with id="myList"
+    list.removeChild(list.childNodes[0]);
+  }
+  console.log("topDeck2 After");
+  console.log(topDeck);
+  console.log("waste After");
+  console.log(waste);
+  topDeck2 = waste;
+  waste = [];
+  joe--;
+  n = 0;
+  console.log("waste" + waste.length);
+  console.log("top" + topDeck2.length);
+}
+
+console.log("waste");
 console.log(waste);
