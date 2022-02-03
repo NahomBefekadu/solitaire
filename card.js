7909e50f91a667cb79f6bb35ee207085de9aaa60
NahomBefekadu <58083518+NahomBefekadu@users.noreply.github.com>
2022-02-02 22:18:06 -0500

    fix win

--- card.js
+++ card.js
@@ -3,26 +3,26 @@ var diamond = document.getElementById("diamond");
 var club = document.getElementById("club");
 var heart = document.getElementById("heart");
 
-if (spade.hasChildNodes()) {
-  var wn1 = spade.children.length;
-}
-if (diamond.hasChildNodes()) {
-  var wn2 = diamond.children.length;
-}
-if (club.hasChildNodes()) {
-  var wn3 = club.children.length;
-}
-if (heart.hasChildNodes()) {
-  var wn4 = heart.children.length;
-}
-var wn5 = wn1 + wn2 + wn3 + wn4;
-
 document.addEventListener(
   "dragend",
   function (event) {
+    if (spade.hasChildNodes()) {
+      var wn1 = spade.children.length;
+    }
+    if (diamond.hasChildNodes()) {
+      var wn2 = diamond.children.length;
+    }
+    if (club.hasChildNodes()) {
+      var wn3 = club.children.length;
+    }
+    if (heart.hasChildNodes()) {
+      var wn4 = heart.children.length;
+    }
+    var wn5 = wn1 + wn2 + wn3 + wn4;
     // reset the transparency
     console.log("Drag event has ended");
-    if (wn5 === 52) {
+    console.log("Drag at", wn5);
+    if (wn5 === 56) {
       window.location.href = "win.html";
       alert("You have won the game you ye bag");
     }
