4e7106e9c84d30e5dd5e817abba8b995965706a9
NahomBefekadu <58083518+NahomBefekadu@users.noreply.github.com>
2022-02-02 20:13:05 -0500

    win condition

---
+++ card.js
@@ -0,0 +1,21 @@
+var spade = document.getElementById("spade");
+var diamond = document.getElementById("diamond");
+var club = document.getElementById("club");
+var heart = document.getElementById("heart");
+
+if (spade.hasChildNodes()) {
+  var wn1 = spade.children.length;
+}
+if (diamond.hasChildNodes()) {
+  var wn2 = diamond.children.length;
+}
+if (club.hasChildNodes()) {
+  var wn3 = club.children.length;
+}
+if (heart.hasChildNodes()) {
+  var wn4 = heart.children.length;
+}
+var wn5 = wn1 + wn2 + wn3 + wn4;
+if (wn5 === 52) {
+  window.location.href = "win.html";
+}
