65cce405bfae3b550221c734114eb76bf57f9ecb
NahomBefekadu <58083518+NahomBefekadu@users.noreply.github.com>
2022-02-03 19:10:40 -0500

    dbl click

--- card.js
+++ card.js
@@ -23,9 +23,37 @@ document.addEventListener(
     // reset the transparency
     console.log("Drag event has ended");
     console.log("Drag at", wn5);
-    if (wn5 ===56) {
+    if (wn5 === 56) {
       //
-      alert("Congratulations you won!\nYour score is " + numMove*150);
+      alert("Congratulations you won!\nYour score is " + numMove * 150);
+      window.location.href = "win.html";
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "dblclick",
+  function (event) {
+    if (spade.hasChildNodes()) {
+      var wn11 = spade.children.length;
+    }
+    if (diamond.hasChildNodes()) {
+      var wn22 = diamond.children.length;
+    }
+    if (club.hasChildNodes()) {
+      var wn33 = club.children.length;
+    }
+    if (heart.hasChildNodes()) {
+      var wn44 = heart.children.length;
+    }
+    var wn55 = wn11 + wn22 + wn33 + wn44;
+    // reset the transparency
+    console.log("double click event has ended");
+    console.log("double click at", wn55);
+    if (wn55 === 56) {
+      //
+      alert("Congratulations you won!\nYour score is " + numMove * 150);
       window.location.href = "win.html";
     }
   },
